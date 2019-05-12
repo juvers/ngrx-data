6c8cb1498372fad68f8a5746e6ea9d49940f475b
Jude Kuti <jkuti@sei-mi.com>
2019-05-12 17:17:45 -0500

    Create entitymetadata and pluralNames and export as entityConfig

--- src/app/store/entity-metatdata.ts
+++ src/app/store/entity-metatdata.ts
@@ -1,2 +1,15 @@
-export interface EntityMetatdata {
-}
+import { EntityMetadataMap } from 'ngrx-data';
+
+const entityMetatdata: EntityMetadataMap = {
+    Hero: {},
+    Villian: {}
+};
+
+const pluralNames = {
+    Hero: 'Heroes'
+};
+
+export const entityConfig = {
+    entityMetatdata,
+    pluralNames
+};
