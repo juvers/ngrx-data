4a88ed803d5c8e9bbea561924bead3de30fb4749
Jude Kuti <jkuti@sei-mi.com>
2019-05-12 17:08:47 -0500

    Set up ngrx by importing store, effects and the dev tools

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    environment.production ? [] : StoreDevtoolsModule.instrument()
   ]
 })
 export class AppStoreModule { }
